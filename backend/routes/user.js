f4e23644b0c84029e7bb409135892b7cd4247bcc
Barbara Gaweł-Kucab <bsgk@interia.pl>
2022-05-03 17:26:16 +0200

    add user update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -189,8 +189,50 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+
 // UPDATE
-// TODO 
+// router.put('/:id', [verifyUser([RoleEnum.ADMIN]), async (req, res) => {
+router.put('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+
+        if (!mongoose.isValidObjectId(id)) {
+            res.status(404).send("Can't find user with given id")
+            return
+        }
+
+        const body = req.body
+        let validationFailed = false
+
+        const user = await User.findByIdAndUpdate(id, { $set: body },
+            { new: true, runValidators: true }).lean()
+        // .catch(err => {
+        //     if (err) {
+        //         res.status(400).send("Data validation failed")
+        //         validationFailed = true
+        //     }
+        // })
+
+        if (validationFailed)
+            return
+
+
+        if (user !== null) {
+            res.status(200).json({
+                message: "User updated successfully",
+                user: user
+            })
+        }
+        else {
+            res.status(404).send("Can't user room with given id")
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send("Something went wrong")
+    }
+    // }])
+})
 
 
 module.exports = router;
