0cafdff3eab1004a7c5d2c5a4dff873a631e755a
Barbara Gaweł-Kucab <bsgk@interia.pl>
2022-05-14 16:16:49 +0200

    small change in delete user option

--- frontend/src/app/components/delete-user/delete-user.component.ts
+++ frontend/src/app/components/delete-user/delete-user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Rate } from 'src/app/models/rate';
 import { User } from 'src/app/models/user';
+import { RatingService } from 'src/app/services/rating.service';
 import { ReservationsService } from 'src/app/services/reservations.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -11,9 +13,11 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class DeleteUserComponent implements OnInit {
   usersList: User[] = []
+  ratingsList: Rate[] = []
   loading: boolean = true
+  loading2: boolean = true
 
-  constructor(public router: Router, private userService: UserService, private reservationService: ReservationsService) {
+  constructor(public router: Router, private userService: UserService, private reservationService: ReservationsService, private ratingService: RatingService) {
     userService.getLoading$().subscribe(
       next => {
         this.loading = next
@@ -23,17 +27,35 @@ export class DeleteUserComponent implements OnInit {
     userService.getUsers().subscribe(next => {
       this.usersList = next
     })
+
+    ratingService.getLoading$().subscribe(
+      next => {
+        this.loading2 = next
+      }
+    )
+
+    ratingService.getRatings().subscribe(next => {
+      this.ratingsList = next
+    })
   }
 
   ngOnInit(): void {
   }
 
-  // delete user and all of his reservations
+  deleteUserRate(id: string) {
+    for (let rating of this.ratingsList) {
+      if (rating.user === id)
+        this.ratingService.removeRating(rating._id)
+    }
+  }
+
+
+  // delete user, user reservations, user rating
   deleteUser(user: User) {
     for (let reservation of user.reservations) {
       this.reservationService.removeReservation(reservation.reservation, user._id)
     }
-
+    this.deleteUserRate(user._id)
     this.userService.removeUser(user._id)
   }
 
